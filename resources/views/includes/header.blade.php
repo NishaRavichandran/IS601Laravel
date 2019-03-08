a1058a1a9aba8cb95ab7f24805a8dfd1c8c23170
nisha <nr437@njit.edu>
2019-03-08 15:57:16 -0500

    Task: Added a template from bootstrap for about page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -36,10 +36,10 @@
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="about">About</a>
+                <a class="nav-link" href="/about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contact">Contact</a>
+                <a class="nav-link" href="/contact">Contact</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
